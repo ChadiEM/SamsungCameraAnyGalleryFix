8d91d4eaf8797066655a197756d684a770a7381b
Marcel Dopita <m@rcel.cz>
2014-12-30 17:30:10 +0100

    Initial commit

---
+++ app/src/main/java/com/smartmadsoft/xposed/samsungcameragallery/Selfie.java
@@ -0,0 +1,43 @@
+package com.smartmadsoft.xposed.samsungcameragallery;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class Selfie implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
+        if (!lpparam.packageName.equals("com.sec.android.app.camera"))
+            return;
+
+        XposedBridge.hookAllMethods(XposedHelpers.findClass("com.sec.android.app.camera.Camera", lpparam.classLoader), "onLaunchGalleryForImage", new XC_MethodReplacement() {
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+
+                Context context = (Context) param.thisObject;
+                ContentResolver contentResolver = context.getContentResolver();
+
+                Object mEngine = XposedHelpers.getObjectField(context, "mEngine");
+                Uri lastUri = (Uri) XposedHelpers.callMethod(mEngine, "getLastContentUri");
+                String mime = contentResolver.getType(lastUri);
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.setDataAndType(lastUri, mime);
+                context.startActivity(intent);
+
+                return null;
+            }
+        });
+    }
+}
