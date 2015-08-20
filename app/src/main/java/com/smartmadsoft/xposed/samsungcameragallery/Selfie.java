12a7dde5b2ed6fa3d101490c5a8e6fec66236cbd
Chadi El Masri <chadimsr@gmail.com>
2015-08-21 00:35:23 +0300

    Fixed for my own version

--- app/src/main/java/com/smartmadsoft/xposed/samsungcameragallery/Selfie.java
+++ app/src/main/java/com/smartmadsoft/xposed/samsungcameragallery/Selfie.java
@@ -19,21 +19,28 @@ public class Selfie implements IXposedHookLoadPackage {
         if (!lpparam.packageName.equals("com.sec.android.app.camera"))
             return;
 
-        XposedBridge.hookAllMethods(XposedHelpers.findClass("com.sec.android.app.camera.Camera", lpparam.classLoader), "onLaunchGalleryForImage", new XC_MethodReplacement() {
+        XposedBridge.hookAllMethods(XposedHelpers.findClass("com.sec.android.app.camera.Camera", lpparam.classLoader), "onLaunchGallery", new XC_MethodReplacement() {
             @Override
             protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
 
                 Context context = (Context) param.thisObject;
                 ContentResolver contentResolver = context.getContentResolver();
+				
+				XposedBridge.log("Context retrieved.");
 
-                Object mEngine = XposedHelpers.getObjectField(context, "mEngine");
+                Object mEngine = XposedHelpers.getObjectField(context, "mCameraEngine");
                 Uri lastUri = (Uri) XposedHelpers.callMethod(mEngine, "getLastContentUri");
                 String mime = contentResolver.getType(lastUri);
+				
+				XposedBridge.log("URI retrieved: " + lastUri);
 
                 Intent intent = new Intent();
                 intent.setAction("com.android.camera.action.REVIEW");
                 intent.addCategory(Intent.CATEGORY_DEFAULT);
                 intent.setDataAndType(lastUri, mime);
+				
+				XposedBridge.log("Starting activity");
+				
                 context.startActivity(intent);
 
                 return null;
